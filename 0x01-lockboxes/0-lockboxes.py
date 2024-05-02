589c761cceb5cd838d1961e2b65ddc68d7e80e4c
noahwekesa <noahwekesa0@gmail.com>
2024-05-02 16:35:25 +0300

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,36 +6,16 @@ module to deTERMINE if all boxes can be opened
 
 def canUnlockAll(boxes):
     """
-    This function determines if all the boxes can be opened.
-
-    Args:
-        boxes: A list of lists, where each inner list represents the keys that can open the corresponding box.
-
-    Returns:
-        True if all boxes can be opened, False otherwise.
+    Function to determine if all boxes can be opened
     """
 
-    # Initialize a visited set to track opened boxes
-    visited = set([0])  # Start with box 0 as initially unlocked
+    no_of_keys = [0]
+    no_of_boxes = len(boxes)
 
-    # Loop through all boxes
-    for i in range(len(boxes)):
-        # Skip processing already visited boxes
-        if i not in visited:
-            continue
-
-        # Check if all keys in the current box lead to opened boxes
-        can_open_all = True
-        for key in boxes[i]:
-            # Check key validity
-            if key not in visited and 0 <= key < len(boxes):
-                can_open_all = False
-                break
-        visited.update(boxes[i])  # Add newly opened boxes to visited set
-
-        # If any key couldn't open a box, return False
-        if not can_open_all:
-            return False
-
-    # If all boxes are visited (opened), return True
-    return True
+    for keys in no_of_keys:
+        for box in boxes[keys]:
+            if box < no_of_boxes and box not in no_of_keys:
+                no_of_keys.append(box)
+    if len(no_of_keys) == no_of_boxes:
+        return True
+    return False
