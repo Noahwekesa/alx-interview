774b1cc25639aa5d81bf23189f40ae1057502d9a
noahwekesa <noahwekesa0@gmail.com>
2024-06-21 17:18:18 +0300

    fixed issue

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,7 +4,7 @@ Making Change
 """
 
 
-def makingChange(coins, total):
+def makeChange(coins, total):
     """
     Given a pile of coins of different values, determine the fewest number
     of coins needed to meet a given amount total.
